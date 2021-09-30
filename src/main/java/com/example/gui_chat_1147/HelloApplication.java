3762ac9a902b86d41b111cafbe95535f02861993
Vladlen <vladlen@vozhzhaev.ru>
2021-09-30 10:00:38 +0300

    Финальная версия чата

--- src/main/java/com/example/gui_chat_1147/HelloApplication.java
+++ src/main/java/com/example/gui_chat_1147/HelloApplication.java
@@ -1,9 +1,11 @@
 package com.example.gui_chat_1147;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
@@ -11,10 +13,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 250, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
         stage.setTitle("Клиент чата");
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(e->{
+            Platform.exit();
+            System.exit(0);
+        });
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/gui_chat_1147/HelloController.java
+++ src/main/java/com/example/gui_chat_1147/HelloController.java
@@ -1,28 +1,81 @@
 package com.example.gui_chat_1147;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class HelloController {
+    DataOutputStream out;
+    ArrayList<String> usersName = new ArrayList<>();
     @FXML
     private TextArea textArea;
     @FXML
     private TextField textField;
-
+    @FXML
+    private TextArea onlineUsersTextArea;
+    @FXML
+    private Button connectBtn;
     @FXML // Обработчк кнопки отправить
     protected void handlerSend(){
         String text = textField.getText();
+        try {
+            out.writeUTF(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         textArea.appendText(text+"\n");// Нужно добавлять текст, а не заменять
         textField.clear(); // Очистить textField
     }
-
     @FXML
-    private Label welcomeText;
+    protected void handlerConnection(){
+        try {
+            Socket socket = new Socket("127.0.0.1", 8188);
+            // Поток ввода
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            // Поток вывода
+            out = new DataOutputStream(socket.getOutputStream());
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true){
+                            String response = "";
+                            Object object = ois.readObject();
+                            System.out.println(object.getClass());
+                            if(object.getClass().equals(usersName.getClass())){
+                                usersName = (ArrayList<String>) object;
+                                System.out.println(usersName);
+                                onlineUsersTextArea.clear();
+                                for(String userName : usersName){
+                                    onlineUsersTextArea.appendText(userName+"\n");
+                                }
+                            }else{
+                                response = object.toString();
+                                textArea.appendText(response+"\n");
+                            }
+                            /*String response = in.readUTF();*/
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                        System.out.println("Соединение разорвано");
+                    }
+                }
+            });
+            thread.start();
+            connectBtn.setDisable(true);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
     }
+
 }
